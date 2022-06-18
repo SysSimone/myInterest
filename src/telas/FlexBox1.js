dc39c5e3a7884d940cd72215b4beec02e0b8d1e5
SysSimone <developer.desenv@gmail.com>
2022-06-17 21:24:02 -0300

    atualizações

--- src/telas/FlexBox1.js
+++ src/telas/FlexBox1.js
@@ -1,24 +1,24 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
 
-const FlexBox1 = (props) => {
- 
-  return (               
-    <View style={[{      
-      flexDirection: "row", //por default ela é "row"
-    }]}>                        
-        <View style={{flex:4, backgroundColor:"yellow"}}>
-      <View style={{ flex: 1, backgroundColor: "red" }} />
-      <View style={{ flex: 2, backgroundColor: "darkorange" }} />
-      <View style={{ flex: 3, backgroundColor: "green" }} />
-      </View>
+const FlexBox1 = () => {
+
+  return (
+    <View style={styles.container}>
+      <View style={{ flex: 5, backgroundColor: "yellow" }} />
+      <View style={{ flex: 5, backgroundColor: "red" }} />
+      <View style={{ flex: 5, backgroundColor: "darkorange" }} />
+      <View style={{ flex: 5, backgroundColor: "green" }} />
     </View>
+
   );
 };
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 20,
+    padding: 30,
+    flexDirection: "row",
+    backgroundColor: "#8fbc8f"
   },
 });
 
